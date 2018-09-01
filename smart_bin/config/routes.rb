83f2412757b0903895c8c37df5be6b6e10590520
FrantaF <frantafriedl@seznam.cz>
2018-09-01 10:13:46 +0800

    created controller login, added figaro and font-awesome gems, navbar and footer in application.html.erb, set up root route

--- smart_bin/config/routes.rb
+++ smart_bin/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  root 'static#index'
-  
-  devise_for :users
+   root 'login#login'
 
-  resources :dustbins
+   devise_for :users
+
+   resources :dustbins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
